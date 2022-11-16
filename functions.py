6a94f601d7c2b984aff47a2a37f78a77f42b301c
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-11-15 18:58:16 -0700

    Get user number

---
+++ functions.py
@@ -0,0 +1,10 @@
+# conversion calculator
+# By: Mariel Johnson
+
+# user input regarding the length to convert
+# get the unit from the user
+# convert the length to the correct unit
+# output the answer to the user
+
+user_number = input("What number would you like to convert? ")
+print(user_number)
