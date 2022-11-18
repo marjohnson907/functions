1fa274f73541a6c3d3510feaf67b94178e5eb7a1
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-11-17 18:31:11 -0700

    error checking for unit & #

--- functions.py
+++ functions.py
@@ -6,24 +6,34 @@
 # convert the length to the correct unit
 # output the answer to the user
 
-user_number = float(input("What number would you like to convert? "))
-user_unit = input("What unit is your number? ")
+while True: 
+    while True:
+        user_number = input("What number would you like to convert? ")
+        if user_number.isdigit():
+            user_number = float(user_number)
+            break
+        else:
+            print('Please use a number.')
+            
+    user_unit = input("What unit is your number? ")
 
-# to convert inches to mm, take inches multiplied by 25.4
-# to convert mm to inches, take mm divide by 25.4
+        # to convert inches to mm, take inches multiplied by 25.4
+        # to convert mm to inches, take mm divide by 25.4
 
-# user gives inches unit
+        # user gives inches unit
 
-if(user_unit == 'in'):
-    conv_number = user_number * 25.4
-    conv_unit = 'mm'
-    #perform calculation in to mm
-elif(user_unit == 'mm'):
-    conv_number = user_number / 25.4
-    conv_unit = 'in'
-    #perform mm to in calc
-else:
-    print('That is not a valid unit.')
+    if(user_unit == 'in'):
+        conv_number = user_number * 25.4
+        conv_unit = 'mm'
+        break
+        #perform calculation in to mm
+    elif(user_unit == 'mm'):
+        conv_number = user_number / 25.4
+        conv_unit = 'in'
+        break
+        #perform mm to in calc
+    else:
+        print('That is not a valid unit.')
 
 print(conv_number, conv_unit)
 
