74230c7e0deb1ac78387f7fb2621fc51b39fe643
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-11-22 18:29:36 -0700

    print out in and mm conversion

--- functions.py
+++ functions.py
@@ -5,55 +5,53 @@
 # get the unit from the user
 # convert the length to the correct unit
 # output the answer to the user
-def user_parser(user_input):
-    #Do something
-    #separate the number from the unit
-    values = user_input.rsplit(" ")
+valid_data = True
 
+def user_parser(user_input):
+    global valid_data
+    valid_data = True
+    values = user_input.rsplit(" ")
     number = values[0]
+    
     if number.isdigit():
         number = float(number)
     else: 
         print("That is not a valid number.")
-    
+        valid_data = False
     unit = values[1]
-    if unit != 'in':
+    if unit != 'in' and unit != 'mm':
         print("That is not a valid unit.")
+        valid_data = False
 
     return number, unit
 
-while True: 
-    user_input = input("Number and unit to convert ")
-    user_number, user_unit = user_parser(user_input)
-    print('User number', user_number)
-    print('User unit', user_unit)
-    # while True:
-    #     user_number = input("What number would you like to convert? ")
-    #     if user_number.isdigit():
-    #         user_number = float(user_number)
-    #         break
-    #     else:
-    #         print('Please use a number.')
-            
-    # user_unit = input("What unit is your number? ")
+while True: # continue program until user exits
+    while True: # check for valid data
+        user_input = input("Number and unit to convert ")
+        user_number, user_unit = user_parser(user_input)
+        # check if there are invalid messages
+        if valid_data:
+            print('User number', user_number)
+            print('User unit', user_unit)
+    # perform calculations
+    if(user_unit == 'in'):
+        conv_number = user_number * 25.4
+        conv_unit = 'mm'
+        #perform calculation in to mm
+    elif(user_unit == 'mm'):
+        conv_number = user_number / 25.4
+        conv_unit = 'in'
+    print(conv_number, conv_unit)    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-    #     # to convert inches to mm, take inches multiplied by 25.4
-    #     # to convert mm to inches, take mm divide by 25.4
-
-    #     # user gives inches unit
-
-    # if(user_unit == 'in'):
-    #     conv_number = user_number * 25.4
-    #     conv_unit = 'mm'
-    #     break
-    #     #perform calculation in to mm
-    # elif(user_unit == 'mm'):
-    #     conv_number = user_number / 25.4
-    #     conv_unit = 'in'
-    #     break
-    #     #perform mm to in calc
-    # else:
-    #     print('That is not a valid unit.')
-
-# print(conv_number, conv_unit)
 
