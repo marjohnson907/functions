bf57c4aafec993b08cbe89ac72a8e4e2a5016106
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-11-22 19:02:13 -0700

    formatting print statement

--- functions.py
+++ functions.py
@@ -10,6 +10,7 @@ valid_data = True
 def user_parser(user_input):
     global valid_data
     valid_data = True
+    #TODO address input from user without space
     values = user_input.rsplit(" ")
     number = values[0]
     
@@ -19,11 +20,17 @@ def user_parser(user_input):
         print("That is not a valid number.")
         valid_data = False
     unit = values[1]
-    if unit != 'in' and unit != 'mm':
+    if unit != 'in' and unit != 'mm' and unit != 'ft':
         print("That is not a valid unit.")
         valid_data = False
 
     return number, unit
+
+def print_results(user_number, user_unit, conv_number, conv_unit):
+    user_number = user_number
+    result = ("{:.2f} {} is equal to {:.2f} {}")
+    print(result.format(user_number, user_unit, conv_number, conv_unit))
+    
 
 while True: # continue program until user exits
     while True: # check for valid data
@@ -31,8 +38,6 @@ while True: # continue program until user exits
         user_number, user_unit = user_parser(user_input)
         # check if there are invalid messages
         if valid_data:
-            print('User number', user_number)
-            print('User unit', user_unit)
             break
     # perform calculations
     if(user_unit == 'in'):
@@ -42,7 +47,15 @@ while True: # continue program until user exits
     elif(user_unit == 'mm'):
         conv_number = user_number / 25.4
         conv_unit = 'in'
-    print(conv_number, conv_unit)    
+    elif(user_unit == 'ft'):
+        # perform ft to in
+        conv_number = user_number * 12
+        conv_unit = 'in'
+    # Create a function that prints out the answer formatted to 2 decimal points
+    # give the original value and unit and conv value and unit
+    # 12.00 ft is equal to 144.00 in
+
+    print_results(user_number, user_unit, conv_number,conv_unit)    
     
     
     
